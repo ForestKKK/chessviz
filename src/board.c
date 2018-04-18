9002746f37f4215d4cddedfae01372c466b5fe2f
Илья <Grinch@localhost.localdomain>
2018-04-18 14:40:28 +0700

    Add second cycle 'for'

--- src/board.c
+++ src/board.c
@@ -15,4 +15,12 @@ char **board()
         for (j = 0; j < 9; j++) {
             arr[i][j] = ' ';
         }
-}
+    }
+
+    char g1[]={'r','n','b','q','k','b','n','r'};
+    char p = 'p';
+    
+    for (i = 0; i < 8; i++) {
+        arr[i][0] = 56 -i;
+    }
+    
